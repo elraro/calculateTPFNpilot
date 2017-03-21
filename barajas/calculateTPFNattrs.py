23c005b956bb53886716b7d4da3fd5b4c5052272
elraro <elraro@elraro.eu>
2017-03-21 11:50:49 +0100

    Calculate TP FN from Age, Document, Country and Sex

--- barajas/calculateTPFNattrs.py
+++ barajas/calculateTPFNattrs.py
@@ -16,11 +16,109 @@ country = ["'ARG'", "'AUS'", "'BRA'", "'CHL'", "'COL'", "'ESP'", "'MAR'", "'PER'
 umbrals = [10, 40, 50, 60, 70, 80, 90]
 
 # sex
-for s in sex:
+# for s in sex:
+#     fn_rate = np.empty(shape=0)
+#     tp_rate = np.empty(shape=0)
+#     # ScoreDactilar
+#     cur.execute("SELECT `ScoreReconocimientoFacialVivoChip` FROM `completados` WHERE Sexo=" + s + ";")
+#     data = cur.fetchall()
+#     data = np.asarray(data)
+#     for umbral in umbrals:
+#         tp = 0
+#         fn = 0
+#         for d in data:
+#             if d[0] >= umbral:
+#                 tp += 1
+#             else:
+#                 fn += 1
+#         try:
+#             fn_rate_eer = fn / (fn + tp)
+#         except ZeroDivisionError:
+#             fn_rate_eer = 1
+#         try:
+#             tp_rate_eer = tp / (tp + fn)
+#         except ZeroDivisionError:
+#             tp_rate_eer = 1
+#         fn_rate = np.append(fn_rate, fn_rate_eer)
+#         tp_rate = np.append(tp_rate, tp_rate_eer)
+#     print("Sexo: " + s)
+#     print("TP")
+#     print(*tp_rate, sep='\n')
+#     print("FN")
+#     print(*fn_rate, sep='\n')
+#     print("------------------")
+
+
+# document
+# for d in document:
+#     fn_rate = np.empty(shape=0)
+#     tp_rate = np.empty(shape=0)
+#     cur.execute("SELECT `ScoreReconocimientoFacialVivoChip` FROM `completados` WHERE TipoDocumento=" + d + ";")
+#     data = cur.fetchall()
+#     data = np.asarray(data)
+#     for umbral in umbrals:
+#         tp = 0
+#         fn = 0
+#         for d in data:
+#             if d[0] >= umbral:
+#                 tp += 1
+#             else:
+#                 fn += 1
+#         try:
+#             fn_rate_eer = fn / (fn + tp)
+#         except ZeroDivisionError:
+#             fn_rate_eer = 1
+#         try:
+#             tp_rate_eer = tp / (tp + fn)
+#         except ZeroDivisionError:
+#             tp_rate_eer = 1
+#         fn_rate = np.append(fn_rate, fn_rate_eer)
+#         tp_rate = np.append(tp_rate, tp_rate_eer)
+#     print("TipoDocumento" + d)
+#     print("TP")
+#     print(*tp_rate, sep='\n')
+#     print("FN")
+#     print(*fn_rate, sep='\n')
+#     print("------------------")
+
+
+# age
+# fn_rate = np.empty(shape=0)
+# tp_rate = np.empty(shape=0)
+# cur.execute("SELECT `ScoreReconocimientoFacialVivoChip` FROM `completados` WHERE (Edad>=60);")
+# data = cur.fetchall()
+# data = np.asarray(data)
+# for umbral in umbrals:
+#     tp = 0
+#     fn = 0
+#     for d in data:
+#         if d[0] >= umbral:
+#             tp += 1
+#         else:
+#             fn += 1
+#     try:
+#         fn_rate_eer = fn / (fn + tp)
+#     except ZeroDivisionError:
+#         fn_rate_eer = 1
+#     try:
+#         tp_rate_eer = tp / (tp + fn)
+#     except ZeroDivisionError:
+#         tp_rate_eer = 1
+#     fn_rate = np.append(fn_rate, fn_rate_eer)
+#     tp_rate = np.append(tp_rate, tp_rate_eer)
+# print("Edad")
+# print("TP")
+# print(*tp_rate, sep='\n')
+# print("FN")
+# print(*fn_rate, sep='\n')
+# print("------------------")
+
+
+# country
+for c in country:
     fn_rate = np.empty(shape=0)
     tp_rate = np.empty(shape=0)
-    # ScoreDactilar
-    cur.execute("SELECT `ScoreReconocimientoFacialVivoChip` FROM `completados` WHERE Sexo=" + s + ";")
+    cur.execute("SELECT `ScoreReconocimientoFacialVivoChip` FROM `completados` WHERE PaisEmisor=" + c + ";")
     data = cur.fetchall()
     data = np.asarray(data)
     for umbral in umbrals:
@@ -41,7 +139,7 @@ for s in sex:
             tp_rate_eer = 1
         fn_rate = np.append(fn_rate, fn_rate_eer)
         tp_rate = np.append(tp_rate, tp_rate_eer)
-    print("Sexo: " + s)
+    print("Pais" + c)
     print("TP")
     print(*tp_rate, sep='\n')
     print("FN")
