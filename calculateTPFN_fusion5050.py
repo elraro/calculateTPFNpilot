acb9ac3557e9a95cdc3b8025b9a3026ab0664048
elraro <elraro@elraro.eu>
2017-06-07 13:15:05 +0200

    Fusion and normalize

---
+++ calculateTPFN_fusion5050.py
@@ -0,0 +1,49 @@
+import MySQLdb as Mdb
+import numpy as np
+
+# Hardcoded
+DB_HOST = "localhost"
+DB_USER = "frav"
+DB_PASS = "VXxL4UOLvB6wc01Y3Cxi"
+DB_NAME = "piloto_ambos"
+
+con = Mdb.connect(DB_HOST, DB_USER, DB_PASS, DB_NAME)
+cur = con.cursor()
+
+cur.execute(
+    "SELECT (ScoreDactilar + ScoreReconocimientoFacialVivoChip) / 2 as media, ScoreReconocimientoFacialVivoChip, ScoreDactilar FROM completados")
+data = cur.fetchall()
+data = np.asarray(data)
+
+fn_rate = np.empty(shape=0)
+tp_rate = np.empty(shape=0)
+umbrals = [40, 50, 60, 70, 80, 90, 91, 92, 93, 94, 95]
+
+for umbral in umbrals:
+    tp = 0
+    fn = 0
+    for d in data:
+        if d[2] != 0:
+            if d[0] >= umbral:
+                tp += 1
+            else:
+                fn += 1
+        else:
+            if d[1] >= umbral:
+                tp += 1
+            else:
+                fn += 1
+    try:
+        fn_rate_eer = fn / (fn + tp)
+    except ZeroDivisionError:
+        fn_rate_eer = 1
+    try:
+        tp_rate_eer = tp / (tp + fn)
+    except ZeroDivisionError:
+        tp_rate_eer = 1
+    fn_rate = np.append(fn_rate, fn_rate_eer)
+    tp_rate = np.append(tp_rate, tp_rate_eer)
+
+print(*tp_rate, sep='\n')
+print("------------------")
+print(*fn_rate, sep='\n')
---
+++ normalizePiloto.py
@@ -0,0 +1,38 @@
+import MySQLdb as Mdb
+import numpy as np
+
+# Hardcoded
+DB_HOST = "localhost"
+DB_USER = "frav"
+DB_PASS = "VXxL4UOLvB6wc01Y3Cxi"
+DB_NAME = "piloto_ambos"
+HIGH = 70
+LOW = 0
+
+attr = ["ScoreDactilar"]
+
+con = Mdb.connect(DB_HOST, DB_USER, DB_PASS, DB_NAME)
+cur = con.cursor()
+
+for atr in attr:
+    print("Leyendo atributo " + atr)
+    cur.execute("SELECT FechaInicio, Hash, " + atr + " FROM completados")
+    data = cur.fetchall()
+    data = np.asarray(data)
+    l = [float(i) for i in data[:, 2]]
+    print("Convertido lista a float...")
+    minimum = min(l)
+    maximum = max(l)
+    print(minimum)
+    print(maximum)
+    rng = maximum - minimum
+    count = 0
+    print("Vamos a actualizar valores")
+    for val in l:
+        v = HIGH - ((HIGH - LOW) * (maximum - val) / rng)
+        cur.execute(
+            "UPDATE completados SET " + atr + "=" + str(v) + " WHERE FechaInicio='" + data[count][0] + "' AND Hash='" + data[count][1] + "'")
+        con.commit()
+        count += 1
+    print("Atributo listo")
+    print("-----------------------------------------")
